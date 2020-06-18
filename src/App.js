86f5d268b097b6def3cb68511861cc844d7f547d
Civan Erbay <Civan.Erbay@web.de>
2020-06-18 08:45:41 +0200

    Add Stable Form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import Grid from "@material-ui/core/Grid";
+import Form from "./components/Form";
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [messageState, setMessage] = useState({name: "", email:"", message:""})
+
+    const handleChange = event => {  //HandleChange ändert Werte sobald etwas eingetippt wird, also tatsächlich bei jeder einzelnen Eingabe!
+        const value = event.target.value;
+        setMessage({
+            ...messageState,
+            [event.target.name]: value
+        });
+    }
+
+    const handleSubmit = async event => {  //pusht Eingaben ins Backend
+        event.preventDefault()
+        const response = await fetch("http://localhost:8080/api/send", {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(messageState) // body data type must match "Content-Type" header
+        });
+        console.log(response)
+    }
+
+
+
+    return (
+        <div className="App">
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: "100vh" }}
+            >
+                        <Form handleChange={handleChange} handleSubmit={handleSubmit} email={messageState.email}/>
+
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import TextField from "@material-ui/core/TextField";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
+
+export default function Form({handleChange, handleSubmit, email}) {
+    const classes = useStyles();
+
+
+
+    return(
+        <form className={classes.root} noValidate autoComplete="off">
+            <p>{email}</p>
+            <TextField onChange={handleChange} name="name" id="outlined-basic" label="Your Name" variant="outlined" placeholder="Your Name" />
+            <TextField onChange={handleChange} name="email" id="outlined-basic" label="Your E-Mail" variant="outlined" placeholder="Your E-Mail"/>
+            <TextField
+                id="outlined-multiline-static"
+                label="Your Message"
+                onChange={handleChange}
+                name="message"
+                multiline
+                rows={4}
+                variant="outlined"
+            />
+            <Button onClick={handleSubmit}>Submit</Button>
+        </form>
+
+    )
+
+}
